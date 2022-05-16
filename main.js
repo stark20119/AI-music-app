570ff4e0a43d9fd7e89b53d16a5e763841abeb77
stark20119 <95037224+stark20119@users.noreply.github.com>
2022-05-16 19:57:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,79 +1,165 @@
-song1=""
-song2=""
+song = ""
 
-leftWristx=0
-leftWristy=0
+leftWristx = 0
+leftWristy = 0
 
-rightWristx=0
-rightWristy=0
+rightWristx = 0
+rightWristy = 0
 
-scorelftwrist=0
-status1=""
-status2=""
+scoreleftwrist = 0
+scorerightwrist = 0
+
+
 
 
 function preload() {
-song1=loadSound("blanspace.mp3")
-song2=loadSound("radioactive.mp3")
-
-}
-function setup(){
-canvas=createCanvas(600,430)
-canvas.center()
-video=createCapture(VIDEO)
-video.hide()
-posenet=ml5.poseNet(video,modelLoaded)
-posenet.on('pose',gotPoses)
+  song = loadSound("music.mp3")
 
 }
 
-
-function draw(){
-image(video,0,0,600,430)
-status1=song1.isPlaying()
-  status2=song2.isPlaying()
-
-if (scoreleftwrist>0.2) {
-  circle(leftWristx,leftWristy,30)
-song1.stop()
-if (status2==false) {
-  song2.play()
-  document.getElementById("display").innerHTML="played-Radioactive"
-  
-}
-
-  
-}
-
-
+function setup() {
+  canvas = createCanvas(600, 500)
+  canvas.center()
+  video = createCapture(VIDEO)
+  video.hide()
+  posenet = ml5.poseNet(video, modelLoaded)
+  posenet.on('pose', gotPoses)
 
 
 }
-function modelLoaded(){
-    console.log("posenet is loaded!!")
-    
-    
-    }
-    
-    function gotPoses(results){
-      if (results.length>0) {
-        console.log(results)
-        leftWristx=results[0].pose.leftWrist.x
-        leftWristy=results[0].pose.leftWrist.y
-    
-        rightWristx=results[0].pose.rightWrist.x
-        rightWristy=results[0].pose.rightWrist.y
-        scoreleftwrist=results[0].pose.keypoints[9].score
 
-    
-    console.log(leftWristx,leftWristy,rightWristx,rightWristy)
 
-      } 
-      
-    
-    
-    
-    
+function draw() {
+  image(video, 0, 0, 600, 500)
+
+  fill("black")
+  stroke("white")
+
+  if (scoreleftwrist > 0.2) {
+    circle(leftWristx, leftWristy, 30)
+
+    inno = Number(leftWristy)
+    floornum = floor(inno)
+
+    volume = floornum / 500
+    song.setVolume(volume)
+
+    document.getElementById("volume").innerHTML = "volume=" + volume
+
+
+  }
+
+  if (scorerightwrist > 0.2) {
+    circle(rightWristx, rightWristy, 30)
+
+
+    if (rightWristy > 0 && rightWristy <= 100) {
+      song.rate(0.5)
+      document.getElementById("speed").innerHTML = "speed=0.5x"
+
+    } else if (rightWristy > 100 && rightWristy <= 200) {
+      song.rate(1)
+      document.getElementById("speed").innerHTML = "speed=1x"
+
     }
 
+    else if (rightWristy > 200 && rightWristy <= 300) {
+      song.rate(1.5)
+      document.getElementById("speed").innerHTML = "speed=1.5x"
 
+    }
+
+    else if (rightWristy > 300 && rightWristy <= 400) {
+      song.rate(2)
+      document.getElementById("speed").innerHTML = "speed=2x"
+
+    }
+
+    else if (rightWristy > 400 && rightWristy <= 500) {
+      song.rate(2.5)
+      document.getElementById("speed").innerHTML = "speed=2.5x"
+
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+}
+
+
+
+
+
+
+
+function songply() {
+  song.play()
+
+
+}
+
+function modelLoaded() {
+  console.log("posenet is loaded!!")
+
+
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results)
+    leftWristx = results[0].pose.leftWrist.x
+    leftWristy = results[0].pose.leftWrist.y
+
+    rightWristx = results[0].pose.rightWrist.x
+    rightWristy = results[0].pose.rightWrist.y
+
+    scoreleftwrist = results[0].pose.keypoints[9].score
+    scorerightwrist = results[0].pose.keypoints[10].score
+
+
+    console.log(leftWristx, leftWristy, rightWristx, rightWristy)
+  }
+
+
+
+
+
+}
